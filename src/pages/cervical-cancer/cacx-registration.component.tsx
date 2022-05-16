4e17c47e37c703f920e9eaf87329d0c2dd3593a2
pirupius <piruville@gmail.com>
2022-05-16 13:36:38 +0300

    Adds cacx resources and adds extra config templates

---
+++ src/pages/cervical-cancer/cacx-registration.component.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./cacx.scss";
+
+const CaCxRegistration: React.FC<{ patientUuid: string }> = ({
+  patientUuid,
+}) => {
+  return <>CaCxRegistration</>;
+};
+
+export default CaCxRegistration;
---
+++ src/pages/cervical-cancer/cacx-screening.component.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./cacx.scss";
+
+const CaCxScreening: React.FC<{ patientUuid: string }> = ({ patientUuid }) => {
+  return <>CaCxScreening</>;
+};
+
+export default CaCxScreening;
