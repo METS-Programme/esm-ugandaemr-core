58e373f9ba731f22be537deeed1900eae9905426
Pius Rubangakene <piruville@gmail.com>
2023-05-23 11:45:56 +0300

    (chore) migrate to community form engine (#29)

--- packages/esm-family-health-app/src/index.ts
+++ packages/esm-family-health-app/src/index.ts
@@ -3,7 +3,7 @@ import { configSchema } from './config-schema';
 import ugandaEmrOverrides from './ugandaemr-configuration-overrrides.json';
 import ugandaEmrConfig from './ugandaemr-config';
 import formsRegistry from './forms/forms-registry';
-import { addToBaseFormsRegistry } from '@ohri/openmrs-ohri-form-engine-lib';
+import { addToBaseFormsRegistry } from '@openmrs/openmrs-form-engine-lib';
 import { createDashboardGroup, createDashboardLink } from '@openmrs/esm-patient-common-lib';
 import {
   mchDashboardMeta,
