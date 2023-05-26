7dd86c6bab2d46597a2813615504366684897bd3
kyampeire Hadijah <30952856+hadijahkyampeire@users.noreply.github.com>
2023-05-26 09:38:39 +0300

    - Implement the table view of queues (#34)

---
+++ packages/esm-patient-queues-app/src/active-visits/patient-queues.resource.ts
@@ -0,0 +1,64 @@
+import dayjs from 'dayjs';
+import useSWR from 'swr';
+
+import { formatDate, openmrsFetch, parseDate } from '@openmrs/esm-framework';
+import { useQueueLocations } from '../patient-search/hooks/useQueueLocations';
+import { PatientQueue, UuidDisplay } from '../types/patient-queues';
+
+export interface MappedPatientQueueEntry {
+  id: string;
+  name: string;
+  patientAge: number;
+  patientSex: string;
+  patientDob: string;
+  patientUuid: string;
+  priority: string;
+  priorityComment: string;
+  status: string;
+  waitTime: string;
+  locationFrom?: string;
+  visitNumber: string;
+  identifiers: Array<UuidDisplay>;
+}
+
+export function usePatientQueuesList(locationUuid: string) {
+  const { queueLocations } = useQueueLocations();
+  const queueLocationUuid = locationUuid ? locationUuid : queueLocations[0]?.id;
+
+  const apiUrl = `/ws/rest/v1/patientqueue?v=full&status=pending&location=${queueLocationUuid}`;
+  const { data, error, isLoading, isValidating, mutate } = useSWR<{ data: { results: Array<PatientQueue> } }, Error>(
+    apiUrl,
+    openmrsFetch,
+  );
+
+  const mapppedQueues = data?.data?.results.map((queue: PatientQueue) => {
+    return {
+      ...queue,
+      id: queue.uuid,
+      name: queue.patient.person.display,
+      patientUuid: queue.patient.uuid,
+      priorityComment: queue.priorityComment,
+      priority: queue.priorityComment === 'Urgent' ? 'Priority' : queue.priorityComment,
+      waitTime: queue.dateCreated ? `${dayjs().diff(dayjs(queue.dateCreated), 'minutes')}` : '--',
+      status: queue.status,
+      patientAge: queue.patient.person?.age,
+      patientSex: queue.patient.person?.gender === 'M' ? 'MALE' : 'FEMALE',
+      patientDob: queue.patient?.person?.birthdate
+        ? formatDate(parseDate(queue.patient.person.birthdate), { time: false })
+        : '--',
+      identifiers: queue.patient?.identifiers,
+      locationFrom: queue.locationFrom.uuid,
+      locationTo: queue.locationTo.uuid,
+      visitNumber: queue.visitNumber,
+    };
+  });
+
+  return {
+    patientQueueEntries: mapppedQueues || [],
+    patientQueueCount: mapppedQueues?.length,
+    isLoading,
+    isError: error,
+    isValidating,
+    mutate,
+  };
+}
