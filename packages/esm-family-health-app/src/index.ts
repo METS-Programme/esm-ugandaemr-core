259c6ffa38b07810f6a9cc3c35b9f3631d91274f
jabahum <jabajnan8@gmail.com>
2023-07-09 16:41:51 +0300

    ft : migrate to v5

--- packages/esm-family-health-app/src/index.ts
+++ packages/esm-family-health-app/src/index.ts
@@ -1,122 +1,75 @@
-import { getAsyncLifecycle, defineConfigSchema, provide, getSyncLifecycle } from '@openmrs/esm-framework';
-import { configSchema } from './config-schema';
-import ugandaEmrOverrides from './ugandaemr-configuration-overrrides.json';
-import ugandaEmrConfig from './ugandaemr-config';
-import formsRegistry from './forms/forms-registry';
-import { addToBaseFormsRegistry } from '@openmrs/openmrs-form-engine-lib';
+import { defineConfigSchema, getAsyncLifecycle, getSyncLifecycle, provide } from '@openmrs/esm-framework';
 import { createDashboardGroup, createDashboardLink } from '@openmrs/esm-patient-common-lib';
-import {
-  mchDashboardMeta,
-  familyHealthDashboardMeta,
-  childHealthDashboardMeta,
-  hivExposedInfantMeta,
-  familyPlanningDashboardMeta,
-} from './dashboard.meta';
+import { addToBaseFormsRegistry } from '@openmrs/openmrs-form-engine-lib';
+import { configSchema } from './config-schema';
 import { moduleName } from './constants';
+import {
+  childHealthDashboardMeta,
+  familyHealthDashboardMeta,
+  familyPlanningDashboardMeta,
+  hivExposedInfantMeta,
+  mchDashboardMeta,
+} from './dashboard.meta';
+import formsRegistry from './forms/forms-registry';
+import ugandaEmrConfig from './ugandaemr-config';
+import ugandaEmrOverrides from './ugandaemr-configuration-overrrides.json';
 
-const importTranslation = require.context('../translations', false, /.json$/, 'lazy');
+export const importTranslation = require.context('../translations', false, /.json$/, 'lazy');
 
-const backendDependencies = {
-  fhir2: '^1.2.0',
-  'webservices.rest': '^2.2.0',
+const options = {
+  featureName: '@ugandaemr/esm-family-health-app',
+  moduleName,
 };
 
-function setupOpenMRS() {
-  const options = {
-    featureName: '@ugandaemr/esm-family-health-app',
-    moduleName,
-  };
-
+// startapp
+export function startupApp() {
   defineConfigSchema(moduleName, configSchema);
   provide(ugandaEmrOverrides);
   provide(ugandaEmrConfig);
   addToBaseFormsRegistry(formsRegistry);
-  return {
-    pages: [],
-    extensions: [
-      {
-        id: 'family-health-clinic-dashboard',
-        slot: 'patient-chart-dashboard-slot',
-        load: getSyncLifecycle(createDashboardGroup(familyHealthDashboardMeta), options),
-        meta: familyHealthDashboardMeta,
-      },
-      // SUB MENU
-      {
-        id: 'mch-dashboard',
-        slot: 'family-health-dashboard-slot',
-        load: getSyncLifecycle(createDashboardLink(mchDashboardMeta), options),
-        meta: mchDashboardMeta,
-      },
-      {
-        id: 'mch-dashboard-summary-ext',
-        slot: 'mch-dashboard-slot',
-        load: getAsyncLifecycle(() => import('./pages/family-health-clinic/mch/mch-summary.component'), {
-          featureName: 'mch-dashboard-summary',
-          moduleName,
-        }),
-        meta: {
-          columnSpan: 4,
-        },
-      },
-      {
-        id: 'child-health-dashboard',
-        slot: 'family-health-dashboard-slot',
-        load: getSyncLifecycle(createDashboardLink(childHealthDashboardMeta), options),
-        meta: childHealthDashboardMeta,
-      },
-      {
-        id: 'child-health-summary-ext',
-        slot: 'child-health-dashboard-slot',
-        load: getAsyncLifecycle(() => import('./pages/family-health-clinic/child-health.component'), {
-          featureName: 'child-health-extension',
-          moduleName,
-        }),
-        meta: {
-          columnSpan: 4,
-        },
-      },
-      {
-        id: 'hiv-exposed-infant-dashboard',
-        slot: 'family-health-dashboard-slot',
-        load: getSyncLifecycle(createDashboardLink(hivExposedInfantMeta), options),
-        meta: hivExposedInfantMeta,
-      },
-      {
-        id: 'hiv-exposed-infant-ext',
-        slot: 'hiv-exposed-infant-slot',
-        load: getAsyncLifecycle(
-          () => import('./pages/family-health-clinic/hiv-exposed-infant/hiv-exposed-infant.component'),
-          {
-            featureName: 'hiv-exposed-infant',
-            moduleName,
-          },
-        ),
-        meta: {
-          columnSpan: 4,
-        },
-      },
-      {
-        id: 'family-planning-dashboard',
-        slot: 'family-health-dashboard-slot',
-        load: getSyncLifecycle(createDashboardLink(familyPlanningDashboardMeta), options),
-        meta: familyPlanningDashboardMeta,
-      },
-      {
-        id: 'family-planning-dashboard-ext',
-        slot: 'family-planning-dashboard-slot',
-        load: getAsyncLifecycle(
-          () => import('./pages/family-health-clinic/family-planning/family-planning.component'),
-          {
-            featureName: 'family-planning',
-            moduleName,
-          },
-        ),
-        meta: {
-          columnSpan: 4,
-        },
-      },
-    ],
-  };
 }
 
-export { backendDependencies, importTranslation, setupOpenMRS };
+// pages
+
+// extensions
+export const familyHealthClinicDashboard = getSyncLifecycle(createDashboardGroup(familyHealthDashboardMeta), options);
+
+export const mchDashboard = getSyncLifecycle(createDashboardLink(mchDashboardMeta), options);
+
+export const mchDashboardSummaryExt = getAsyncLifecycle(
+  () => import('./pages/family-health-clinic/mch/mch-summary.component'),
+  {
+    featureName: 'mch-dashboard-summary',
+    moduleName,
+  },
+);
+
+export const childHealthDashboard = getSyncLifecycle(createDashboardLink(childHealthDashboardMeta), options);
+
+export const childHealthSummaryExt = getAsyncLifecycle(
+  () => import('./pages/family-health-clinic/child-health.component'),
+  {
+    featureName: 'child-health-extension',
+    moduleName,
+  },
+);
+
+export const hivExposedInfantDashboard = getSyncLifecycle(createDashboardLink(hivExposedInfantMeta), options);
+
+export const hivExposedInfantExt = getAsyncLifecycle(
+  () => import('./pages/family-health-clinic/hiv-exposed-infant/hiv-exposed-infant.component'),
+  {
+    featureName: 'hiv-exposed-infant',
+    moduleName,
+  },
+);
+
+export const familyPlanningDashboard = getSyncLifecycle(createDashboardLink(familyPlanningDashboardMeta), options);
+
+export const familyPlanningDashboardExt = getAsyncLifecycle(
+  () => import('./pages/family-health-clinic/family-planning/family-planning.component'),
+  {
+    featureName: 'family-planning',
+    moduleName,
+  },
+);
--- packages/esm-ugandaemr-app/src/index.ts
+++ packages/esm-ugandaemr-app/src/index.ts
@@ -1,41 +1,26 @@
-import { getAsyncLifecycle, defineConfigSchema, provide } from '@openmrs/esm-framework';
-import { configSchema } from './config-schema';
-import ugandaEmrOverrides from './ugandaemr-configuration-overrrides.json';
-import ugandaEmrConfig from './ugandaemr-config';
-import formsRegistry from './forms/forms-registry';
+import { defineConfigSchema, getAsyncLifecycle, provide } from '@openmrs/esm-framework';
 import { addToBaseFormsRegistry } from '@openmrs/openmrs-form-engine-lib';
+import { configSchema } from './config-schema';
 import { moduleName } from './constants';
+import formsRegistry from './forms/forms-registry';
+import ugandaEmrConfig from './ugandaemr-config';
+import ugandaEmrOverrides from './ugandaemr-configuration-overrrides.json';
 
-const importTranslation = require.context('../translations', false, /.json$/, 'lazy');
-
-const backendDependencies = {
-  fhir2: '^1.2.0',
-  'webservices.rest': '^2.2.0',
+const options = {
+  featureName: 'esm-ugandaemr-app',
+  moduleName,
 };
 
-function setupOpenMRS() {
-  const options = {
-    featureName: 'esm-ugandaemr-app',
-    moduleName,
-  };
+export const importTranslation = require.context('../translations', false, /.json$/, 'lazy');
 
+export const cervicalCancerSummaryExtension = getAsyncLifecycle(
+  () => import('./pages/cervical-cancer/cacx-visits/cacx-visits.component'),
+  options,
+);
+
+export function startupApp() {
   defineConfigSchema(moduleName, configSchema);
   provide(ugandaEmrOverrides);
   provide(ugandaEmrConfig);
   addToBaseFormsRegistry(formsRegistry);
-  return {
-    pages: [],
-    extensions: [
-      {
-        id: 'cervical-cancer-summary-ext',
-        slot: 'cacx-visits-slot',
-        load: getAsyncLifecycle(() => import('./pages/cervical-cancer/cacx-visits/cacx-visits.component'), {
-          featureName: 'cervical-cancer-summary-extension',
-          moduleName,
-        }),
-      },
-    ],
-  };
 }
-
-export { backendDependencies, importTranslation, setupOpenMRS };
