eb994c96df7a192e094d0df77dee8fed5545962a
jabahum <jabajnan8@gmail.com>
2023-07-11 06:40:35 +0300

    fix missing dependencies

--- packages/esm-ugandaemr-app/src/views/facilities/set-identifer-dialog.component.tsx
+++ packages/esm-ugandaemr-app/src/views/facilities/set-identifer-dialog.component.tsx
@@ -24,7 +24,7 @@ const SetFacilityIdentifier: React.FC<SetFacilityDialogProps> = ({
   const setIdentifier = useMemo(() => {
     updateSelectedFacilityName(facility);
     updateSelectedFacilityIdentifier(uniqueIdentifier)
-  },[]);
+  },[facility,uniqueIdentifier]);
 
   return (
     <div>
