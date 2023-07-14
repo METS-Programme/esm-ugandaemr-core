f00f9c4c14653d440a294ccb0774c3a431f7bc84
jabahum <jabajnan8@gmail.com>
2023-07-14 18:54:17 +0300

    fix : create facility dashhboard

--- packages/esm-ugandaemr-app/src/dashboard.meta.tsx
+++ packages/esm-ugandaemr-app/src/dashboard.meta.tsx
@@ -4,3 +4,19 @@
 //   config: { columns: 1, type: "grid" },
 //   title: "Test",
 // };
+
+export const facilityListMeta = {
+  name: 'facility-lists',
+  slot: 'facility-ohri-home-dashboard-slot',
+  title: 'Facility List',
+  meta: {
+    name: 'facility-list',
+    slot: 'facility-list-dashboard-slot',
+    dashboardTitle: 'Facility Home',
+    config: {
+      columns: 1,
+      type: 'grid',
+      programme: 'facility-page',
+    },
+  },
+};
