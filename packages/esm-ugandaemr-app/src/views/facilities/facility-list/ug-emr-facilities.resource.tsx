c6c367899ebc52e6c63c6953d64e8c5ad642bdaa
jabahum <jabajnan8@gmail.com>
2023-07-19 09:52:44 +0300

    fix crash

--- packages/esm-ugandaemr-app/src/views/facilities/facility-list/ug-emr-facilities.resource.tsx
+++ packages/esm-ugandaemr-app/src/views/facilities/facility-list/ug-emr-facilities.resource.tsx
@@ -1,5 +1,5 @@
 import useSWR from 'swr';
-import { RegionsResponse } from '../../types';
+import { RegionsResponse } from '../../../types';
 
 export interface FacilityResponse {
   message: string;
