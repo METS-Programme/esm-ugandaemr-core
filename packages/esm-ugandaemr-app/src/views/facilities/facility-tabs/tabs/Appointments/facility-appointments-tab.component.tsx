61e210ef922b8be9c479bccbe5c4974e7ed0e123
Daphne210 <dapnephinerry2@gmail.com>
2023-07-21 13:04:33 +0300

    Adding Appointments Tab Components

---
+++ packages/esm-ugandaemr-app/src/views/facilities/facility-tabs/tabs/Appointments/facility-appointments-tab.component.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Tab, TabList, TabPanel, TabPanels, Tabs } from '@carbon/react';
+import { useTranslation } from 'react-i18next';
+import styles from './appointments.scss';
+
+const FacilityAppointmentsTab: React.FC = () => {
+  const { t } = useTranslation();
+
+  return (
+    <div className={styles.tabContainer}>
+      <span> Coming Soon!!(Under Devlopment)</span>
+    </div>
+  );
+}
+
+export default FacilityAppointmentsTab;
