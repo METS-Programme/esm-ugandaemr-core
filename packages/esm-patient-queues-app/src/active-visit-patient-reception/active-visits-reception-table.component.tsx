3cac5936004e0cd87138206e2127dd5d9846cafb
jabahum <jabajnan8@gmail.com>
2023-07-26 13:35:14 +0300

    create receptionist queue table

---
+++ packages/esm-patient-queues-app/src/active-visit-patient-reception/active-visits-reception-table.component.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ActiveVisitsReceptionTable() {
+  return (
+    <div>
+      <span>Coming soon</span>
+    </div>
+  );
+}
+
+export default ActiveVisitsReceptionTable;
