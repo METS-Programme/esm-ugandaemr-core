effe94de8bd29599e0c9010c857af9713b23f2d3
Daphne <89844768+Daphne210@users.noreply.github.com>
2023-08-08 09:43:59 +0300

    U4x-88 ft/designing facility dashboard (#60)
    * yarn prettier format
    
    * adding the search card and button for creating new dashboard
    
    * Adding the search card and create new dashboard icon
    
    * Adding a modal for entering project details
    
    * Adding the drag and drop component
    
    * Deleting the create new dashboard card
    
    * Renaming Dashboard from Facility Dashboard
    
    * Renaming the backend

--- packages/esm-patient-queues-app/src/active-visits/active-visits-table.component.tsx
+++ packages/esm-patient-queues-app/src/active-visits/active-visits-table.component.tsx
@@ -234,18 +234,18 @@ const ActiveVisitsTable: React.FC<ActiveVisitsTableProps> = ({ status }) => {
         const filterableValue = cellsById[cellId].value;
         const filterTerm = inputValue.toLowerCase();
 
-        if (typeof filterableValue === 'boolean') {
-          return false;
-        }
-        if (filterableValue.hasOwnProperty('content')) {
-          if (Array.isArray(filterableValue.content.props.children)) {
-            return ('' + filterableValue.content.props.children[1].props.children).toLowerCase().includes(filterTerm);
-          }
-          if (typeof filterableValue.content.props.children === 'object') {
-            return ('' + filterableValue.content.props.children.props.children).toLowerCase().includes(filterTerm);
-          }
-          return ('' + filterableValue.content.props.children).toLowerCase().includes(filterTerm);
-        }
+        // if (typeof filterableValue === 'boolean') {
+        //   return false;
+        // }
+        // if (filterableValue.hasOwnProperty('content')) {
+        //   if (Array.isArray(filterableValue.content.props.children)) {
+        //     return ('' + filterableValue.content.props.children[1].props.children).toLowerCase().includes(filterTerm);
+        //   }
+        //   if (typeof filterableValue.content.props.children === 'object') {
+        //     return ('' + filterableValue.content.props.children.props.children).toLowerCase().includes(filterTerm);
+        //   }
+        //   return ('' + filterableValue.content.props.children).toLowerCase().includes(filterTerm);
+        // }
         return ('' + filterableValue).toLowerCase().includes(filterTerm);
       }),
     );
