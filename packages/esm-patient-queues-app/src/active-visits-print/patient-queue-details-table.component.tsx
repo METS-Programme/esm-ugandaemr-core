d66d21c48c20c0dbdabf7c24dec3aaa9f236c3ca
Isaac Obella <wizlif@users.noreply.github.com>
2023-08-16 12:23:06 +0300

    fix: remove unused details

--- packages/esm-patient-queues-app/src/active-visits-print/patient-queue-details-table.component.tsx
+++ packages/esm-patient-queues-app/src/active-visits-print/patient-queue-details-table.component.tsx
@@ -12,28 +12,16 @@ const PatientQueueDetailsTable: React.FC<PrintTableProps> = ({ queueEntry }) => 
     <table>
       <tbody>
         <tr className={styles.name}>
-          <td align="right">Check In Date:&nbsp;</td>
-          <td>{queueEntry.dateCreated}</td>
-        </tr>
-        <tr className={styles.name}>
-          <td align="right">Patient Name:&nbsp;</td>
-          <td>{queueEntry.name}</td>
-        </tr>
-        <tr className={styles.name}>
-          <td align="right">Visit No:&nbsp;</td>
+          <td align="right">Visit No:</td>
           <td>{trimVisitNumber(queueEntry.visitNumber)}</td>
         </tr>
         <tr className={styles.name}>
-          <td align="right">Gender:&nbsp;</td>
-          <td>{queueEntry.patientSex}</td>
-        </tr>
-        <tr className={styles.name}>
-          <td align="right">Entry Point:&nbsp;</td>
+          <td align="right">Entry Point:</td>
           <td>{queueEntry.creatorDisplay}</td>
         </tr>
 
         <tr className={styles.name}>
-          <td align="right">Attendant:&nbsp;</td>
+          <td align="right">Attendant:</td>
           <td>{queueEntry.creatorUsername}</td>
         </tr>
       </tbody>
