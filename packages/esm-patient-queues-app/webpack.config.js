2b2940f698c3f2cf6c30c0aa172dc4806c273f8e
pirupius <piruville@gmail.com>
2023-08-16 17:28:39 +0300

    (fix) fixes to dependecies and webpack

--- packages/esm-patient-queues-app/webpack.config.js
+++ packages/esm-patient-queues-app/webpack.config.js
@@ -1,1 +1,16 @@
-module.exports = require('openmrs/default-webpack-config');
+const path = require('path');
+const config = (module.exports = require('openmrs/default-webpack-config'));
+config.scriptRuleConfig.exclude =
+  path.sep == '/'
+    ? /(node_modules[^\/@openmrs\/esm\-patient\-common\-lib, ^\/@ohri\/openmrs\-esm\-ohri\-commons\-lib])/
+    : /(node_modules[^\\@openmrs\/esm\-patient\-common\-lib, ^\\@ohri\/openmrs\-esm\-ohri\-commons\-lib])/;
+config.overrides.resolve = {
+  extensions: ['.tsx', '.ts', '.jsx', '.js', '.scss'],
+  alias: {
+    '@openmrs/esm-framework': '@openmrs/esm-framework/src/internal',
+    '@openmrs/openmrs-form-engine-lib': '@openmrs/openmrs-form-engine-lib/src/index',
+    '@ohri/openmrs-esm-ohri-commons-lib': '@ohri/openmrs-esm-ohri-commons-lib/src/index',
+  },
+};
+
+module.exports = config;
