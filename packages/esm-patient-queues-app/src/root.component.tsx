09df21bf413daaf7c4f9daf469e6118d7dd2eb9e
Dennis Kigen <kigen.work@gmail.com>
2023-08-26 17:10:57 +0300

    (fix) Fix patient queues dashboard links (#88)
    * (fix) Fix patient queues dashboard links
    
    * Fixup

--- packages/esm-patient-queues-app/src/root.component.tsx
+++ packages/esm-patient-queues-app/src/root.component.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { SWRConfig } from 'swr';
-import ServicesTable from './queue-patient-linelists/queue-services-table.component';
-import AppointmentsTable from './queue-patient-linelists/scheduled-appointments-table.component';
+import Home from './home.component';
 
 const swrConfiguration = {
   errorRetryCount: 3,
@@ -12,10 +11,9 @@ const Root: React.FC = () => {
   return (
     <main>
       <SWRConfig value={swrConfiguration}>
-        <BrowserRouter basename={`${window.spaBase}/home/patient-queues`}>
+        <BrowserRouter basename={`${window.getOpenmrsSpaBase()}` + 'home/patient-queues'}>
           <Routes>
-            <Route path="/" element={<ServicesTable />} />
-            <Route path="/appointments-list/:value/" element={<AppointmentsTable />} />
+            <Route path="/" element={<Home />} />
           </Routes>
         </BrowserRouter>
       </SWRConfig>
