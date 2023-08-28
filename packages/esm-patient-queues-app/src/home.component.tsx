be5ea26bcd65b0639cb94ecc85352d1669ca7e28
jabahum <jabajnan8@gmail.com>
2023-08-28 09:49:39 +0300

    fix : remove unnecessary comments

--- packages/esm-patient-queues-app/src/home.component.tsx
+++ packages/esm-patient-queues-app/src/home.component.tsx
@@ -2,7 +2,6 @@ import { UserHasAccess } from '@openmrs/esm-framework';
 import React from 'react';
 import ActiveVisitsReceptionTable from './active-visit-patient-reception/active-visits-reception-table.component';
 import ActiveVisitsTabs from './active-visits/active-visits-tab.component';
-import ActiveVisitsTable from './active-visits/active-visits-table.component';
 import {
   PRIVILEGE_CLINICIAN_QUEUE_LIST,
   PRIVILEGE_RECEPTION_QUEUE_LIST,
@@ -20,12 +19,9 @@ const Home: React.FC<HomeProps> = (props) => {
       <ClinicMetrics />
       <UserHasAccess privilege={PRIVILEGE_RECEPTION_QUEUE_LIST}>
         <ActiveVisitsReceptionTable />
-        {/* <ActiveVisitsTable status={'pending'} /> */}
-        {/* add  */}
       </UserHasAccess>
       <UserHasAccess privilege={PRIVILEGE_CLINICIAN_QUEUE_LIST}>
-        {/* <ActiveVisitsTabs /> */}
-        <ActiveVisitsTable status={'pending'} />
+        <ActiveVisitsTabs />
       </UserHasAccess>
       <UserHasAccess privilege={PRIVILEGE_TRIAGE_QUEUE_LIST}>
         <ActiveVisitsTabs />
