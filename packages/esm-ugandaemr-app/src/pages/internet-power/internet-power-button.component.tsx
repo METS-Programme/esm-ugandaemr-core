bab466cb891fd60eadb6b46b260132d0b48608c1
Daphne <89844768+Daphne210@users.noreply.github.com>
2023-12-13 00:34:54 +0300

    Modal for internet & power connectivity  (#137)
    * migrate to ugana-emr server
    
    * add dashboard
    
    * fix
    
    * fix
    
    * Demonstrate consuming OHRI DBs
    
    * ft : home
    
    * add dashboard
    
    * fix
    
    * fix
    
    * ft : home
    
    * remove duplicate import
    
    * fix
    
    * ft  : add facilities
    
    * rf : change home metrics tiles view
    
    * fix : change to  clinic visits
    
    * ft:refactor patient queues
    
    * ft : facility registration integration
    
    * fix : change active queue icon
    
    * fix : change actions menu to button
    
    * fix: set dropdown intial value
    
    * fix: remove date,time ,clinic and visittype
    
    * fix : create facility dashhboard
    
    * fix : priorty,priority comment update queue
    
    * fix : add notes
    
    * fix: update index
    
    * fix: action files
    
    * fix : create dashboards
    
    * fix: update files
    
    * Color coding wait time in a queue
    
    * formatted using yarn prettier
    
    * Modal for internet & power connectivity
    
    ---------
    
    Co-authored-by: jabahum <jabajnan8@gmail.com>
    Co-authored-by: Samuel Male <samuelsmalek@gmail.com>
    Co-authored-by: Jabar Jeremy <24471994+jabahum@users.noreply.github.com>

---
+++ packages/esm-ugandaemr-app/src/pages/internet-power/internet-power-button.component.tsx
@@ -0,0 +1,35 @@
+import { Button } from '@carbon/react';
+import React, { useCallback, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { Add } from '@carbon/react/icons';
+import InternetPowerModal from './internet_power.component';
+
+const UpdateInternetPowerButton = () => {
+  const { t } = useTranslation();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const launchInternetPowerModal = useCallback(() => {
+    setIsModalOpen(true);
+  }, []);
+
+  const closeModal = useCallback(() => {
+    setIsModalOpen(false);
+  }, []);
+
+  return (
+    <>
+      <Button
+        kind="ghost"
+        size="sm"
+        onClick={launchInternetPowerModal}
+        renderIcon={(props) => <Add size={16} {...props} />}
+      >
+        <span>Internet & Power Connectivity</span>
+      </Button>
+
+      {isModalOpen && <InternetPowerModal closeModal={closeModal} />}
+    </>
+  );
+};
+
+export default UpdateInternetPowerButton;
