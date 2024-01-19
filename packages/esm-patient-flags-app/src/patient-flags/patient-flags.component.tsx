9fe90cb1b665af59087f0c57fca6759feaaa5aa6
Mwanje Musa <mwanjemusa@gmail.com>
2024-01-19 10:52:23 +0300

    U4X-373:Add patient flags functionality (#152)

---
+++ packages/esm-patient-flags-app/src/patient-flags/patient-flags.component.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Tag } from '@carbon/react';
+import { useTranslation } from 'react-i18next';
+import { usePatientFlags } from '../hooks/usePatientFlags';
+import styles from './patient-flags.scss';
+
+interface PatientFlagsProps {
+  patientUuid: string;
+}
+
+const PatientFlags: React.FC<PatientFlagsProps> = ({ patientUuid }) => {
+  const { t } = useTranslation();
+  const { patientFlags, error } = usePatientFlags(patientUuid);
+
+  if (error) {
+    return <span>{t('errorPatientFlags', 'Error loading patient flags')}</span>;
+  }
+
+  return (
+    <div className={styles.flagContainer}>
+      {patientFlags.map((patientFlag) => (
+        <Tag className={styles.tag} key={patientFlag} type="magenta">
+          {patientFlag?.display}
+        </Tag>
+      ))}
+    </div>
+  );
+};
+
+export default PatientFlags;
