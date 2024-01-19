b11722f4491093a7ae8daf8410879298e38b5797
Mwanje Musa <mwanjemusa@gmail.com>
2024-01-19 10:52:23 +0300

    U4X-373:Add patient flags functionality (#152)

---
+++ packages/esm-patient-flags-app/src/hooks/usePatientFlags.tsx
@@ -0,0 +1,21 @@
+import { openmrsFetch } from '@openmrs/esm-framework';
+import useSWR from 'swr';
+
+export interface Result {
+  uuid: string;
+  display: string;
+  links: Link[];
+}
+
+export interface Link {
+  rel: string;
+  uri: string;
+  resourceAlias: string;
+}
+
+export const usePatientFlags = (patientUuid: string) => {
+  const patientFlagsUrl = `/ws/rest/v1/patientflags/patientflag?patient=${patientUuid}`;
+  const { data, error, isLoading } = useSWR<{ data: { results: Array<Result> } }, Error>(patientFlagsUrl, openmrsFetch);
+  const patientFlags = typeof data?.data === 'string' ? [] : data?.data?.results ?? [];
+  return { patientFlags, isLoading, error };
+};
