e9e86bf6711780a506b7a469d20abe59ec174fd1
jabahum <jabajnan8@gmail.com>
2024-03-04 10:15:58 +0300

    set up program enrollment

---
+++ packages/esm-patient-programs-app/src/programs/programs-detailed-summary.component.tsx
@@ -0,0 +1,182 @@
+import React, { useCallback, useMemo } from 'react';
+import classNames from 'classnames';
+import { useTranslation } from 'react-i18next';
+import {
+  Button,
+  DataTable,
+  type DataTableHeader,
+  DataTableSkeleton,
+  InlineLoading,
+  InlineNotification,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@carbon/react';
+import { Add, Edit } from '@carbon/react/icons';
+import {
+  formatDate,
+  formatDatetime,
+  useConfig,
+  type ConfigObject,
+  useLayoutType,
+  isDesktop as desktopLayout,
+} from '@openmrs/esm-framework';
+import { CardHeader, EmptyState, ErrorState, launchPatientWorkspace } from '@openmrs/esm-patient-common-lib';
+import { usePrograms } from './programs.resource';
+import styles from './programs-detailed-summary.scss';
+
+interface ProgramsDetailedSummaryProps {
+  patientUuid: string;
+}
+
+interface ProgramEditButtonProps {
+  programEnrollmentId: string;
+}
+
+const ProgramsDetailedSummary: React.FC<ProgramsDetailedSummaryProps> = ({ patientUuid }) => {
+  const { t } = useTranslation();
+  const layout = useLayoutType();
+  const isTablet = layout === 'tablet';
+  const isDesktop = desktopLayout(layout);
+  const { hideAddProgramButton } = useConfig<ConfigObject>();
+  const displayText = t('programEnrollments', 'Program enrollments');
+  const headerTitle = t('carePrograms', 'Care Programs');
+
+  const { enrollments, isLoading, isError, isValidating, availablePrograms, eligiblePrograms } =
+    usePrograms(patientUuid);
+
+  const tableHeaders: Array<typeof DataTableHeader> = useMemo(
+    () => [
+      {
+        key: 'display',
+        header: t('activePrograms', 'Active programs'),
+      },
+      {
+        key: 'location',
+        header: t('location', 'Location'),
+      },
+      {
+        key: 'dateEnrolled',
+        header: t('dateEnrolled', 'Date enrolled'),
+      },
+      {
+        key: 'status',
+        header: t('status', 'Status'),
+      },
+    ],
+    [t],
+  );
+
+  const tableRows = useMemo(() => {
+    return enrollments?.map((program) => {
+      return {
+        id: program.uuid,
+        display: program.display,
+        location: program.location?.display ?? '--',
+        dateEnrolled: formatDatetime(new Date(program.dateEnrolled)),
+        status: program.dateCompleted
+          ? `${t('completedOn', 'Completed On')} ${formatDate(new Date(program.dateCompleted))}`
+          : t('active', 'Active'),
+      };
+    });
+  }, [enrollments, t]);
+
+  const launchProgramsForm = useCallback(() => launchPatientWorkspace('programs-form-workspace'), []);
+
+  if (isLoading) return <DataTableSkeleton role="progressbar" compact={isDesktop} zebra />;
+  if (isError) return <ErrorState error={isError} headerTitle={headerTitle} />;
+  if (enrollments?.length) {
+    return (
+      <div className={styles.widgetCard}>
+        <CardHeader title={headerTitle}>
+          <span>{isValidating ? <InlineLoading /> : null}</span>
+          {hideAddProgramButton ? null : (
+            <Button
+              kind="ghost"
+              renderIcon={(props) => <Add size={16} {...props} />}
+              iconDescription="Add programs"
+              onClick={launchProgramsForm}
+              disabled={availablePrograms?.length && eligiblePrograms?.length === 0}
+            >
+              {t('add', 'Add')}
+            </Button>
+          )}
+        </CardHeader>
+        {availablePrograms?.length && eligiblePrograms?.length === 0 ? (
+          <InlineNotification
+            style={{ minWidth: '100%', margin: '0rem', padding: '0rem' }}
+            kind={'info'}
+            lowContrast
+            subtitle={t('noEligibleEnrollments', 'There are no more programs left to enroll this patient in')}
+            title={t('fullyEnrolled', 'Enrolled in all programs')}
+          />
+        ) : null}
+        <DataTable rows={tableRows} headers={tableHeaders} isSortable size={isTablet ? 'lg' : 'sm'} useZebraStyles>
+          {({ rows, headers, getHeaderProps, getTableProps }) => (
+            <TableContainer>
+              <Table aria-label="program enrollments" {...getTableProps()}>
+                <TableHead>
+                  <TableRow>
+                    {headers.map((header) => (
+                      <TableHeader
+                        className={classNames(styles.productiveHeading01, styles.text02)}
+                        {...getHeaderProps({
+                          header,
+                          isSortable: header.isSortable,
+                        })}
+                      >
+                        {header.header?.content ?? header.header}
+                      </TableHeader>
+                    ))}
+                    <TableHeader />
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {rows.map((row, i) => (
+                    <TableRow key={row.id}>
+                      {row.cells.map((cell) => (
+                        <TableCell key={cell.id}>{cell.value?.content ?? cell.value}</TableCell>
+                      ))}
+                      <TableCell className="cds--table-column-menu">
+                        <ProgramEditButton programEnrollmentId={enrollments[i]?.uuid} />
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          )}
+        </DataTable>
+      </div>
+    );
+  }
+  return <EmptyState displayText={displayText} headerTitle={headerTitle} launchForm={launchProgramsForm} />;
+};
+
+function ProgramEditButton({ programEnrollmentId }: ProgramEditButtonProps) {
+  const isTablet = useLayoutType() === 'tablet';
+  const { t } = useTranslation();
+  const launchEditProgramsForm = React.useCallback(
+    () => launchPatientWorkspace('programs-form-workspace', { programEnrollmentId }),
+    [programEnrollmentId],
+  );
+
+  return (
+    <Button
+      aria-label="edit program"
+      kind="ghost"
+      renderIcon={(props) => <Edit size={16} {...props} />}
+      iconDescription={t('editProgram', 'Edit Program')}
+      onClick={launchEditProgramsForm}
+      hasIconOnly
+      tooltipPosition="left"
+      size={isTablet ? 'lg' : 'sm'}
+    />
+  );
+}
+
+export default ProgramsDetailedSummary;
---
+++ packages/esm-patient-programs-app/src/programs/programs-form.test.tsx
@@ -0,0 +1,180 @@
+import React from 'react';
+import { throwError } from 'rxjs';
+import { of } from 'rxjs/internal/observable/of';
+import { render, screen, within } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { createErrorHandler, openmrsFetch, showSnackbar } from '@openmrs/esm-framework';
+import { mockCareProgramsResponse, mockEnrolledProgramsResponse, mockLocationsResponse } from '__mocks__';
+import { createProgramEnrollment, updateProgramEnrollment } from './programs.resource';
+import { mockPatient } from 'tools';
+import ProgramsForm from './programs-form.component';
+
+const testProps = {
+  closeWorkspace: jest.fn(),
+  patientUuid: mockPatient.id,
+  promptBeforeClosing: jest.fn(),
+};
+
+const mockCreateErrorHandler = createErrorHandler as jest.Mock;
+const mockCreateProgramEnrollment = createProgramEnrollment as jest.Mock;
+const mockUpdateProgramEnrollment = updateProgramEnrollment as jest.Mock;
+const mockOpenmrsFetch = openmrsFetch as jest.Mock;
+const mockShowSnackbar = showSnackbar as jest.Mock;
+
+jest.mock('@openmrs/esm-framework', () => {
+  const originalModule = jest.requireActual('@openmrs/esm-framework');
+
+  return {
+    ...originalModule,
+    createErrorHandler: jest.fn(),
+    showSnackbar: jest.fn(),
+    useLocations: jest.fn().mockImplementation(() => mockLocationsResponse),
+  };
+});
+
+jest.mock('./programs.resource', () => {
+  const originalModule = jest.requireActual('./programs.resource');
+
+  return {
+    ...originalModule,
+    createProgramEnrollment: jest.fn(),
+    updateProgramEnrollment: jest.fn(),
+  };
+});
+
+describe('ProgramsForm', () => {
+  xit('renders a success toast notification upon successfully recording a program enrollment', async () => {
+    const user = userEvent.setup();
+
+    const inpatientWardUuid = 'b1a8b05e-3542-4037-bbd3-998ee9c40574';
+    const oncologyScreeningProgramUuid = '11b129ca-a5e7-4025-84bf-b92a173e20de';
+
+    mockOpenmrsFetch.mockReturnValueOnce({ data: { results: mockCareProgramsResponse } });
+    mockOpenmrsFetch.mockReturnValueOnce({ data: { results: mockEnrolledProgramsResponse } });
+    mockCreateProgramEnrollment.mockReturnValueOnce(of({ status: 201, statusText: 'Created' }));
+
+    renderProgramsForm();
+
+    const enrollButton = screen.getByRole('button', { name: /save and close/i });
+    const enrollmentDateInput = screen.getAllByRole('textbox', { name: '' })[0];
+    const selectLocationInput = screen.getAllByRole('combobox', { name: '' })[1];
+    const selectProgramInput = screen.getAllByRole('combobox', { name: '' })[0];
+
+    await user.type(enrollmentDateInput, '2020-05-05');
+    await user.selectOptions(selectProgramInput, [oncologyScreeningProgramUuid]);
+    await user.selectOptions(selectLocationInput, [inpatientWardUuid]);
+
+    expect(screen.getByRole('option', { name: /Inpatient Ward/i })).toBeInTheDocument();
+
+    await user.click(enrollButton);
+
+    expect(mockCreateProgramEnrollment).toHaveBeenCalledTimes(1);
+    expect(mockCreateProgramEnrollment).toHaveBeenCalledWith(
+      expect.objectContaining({
+        dateCompleted: null,
+        location: inpatientWardUuid,
+        patient: mockPatient.id,
+        program: oncologyScreeningProgramUuid,
+      }),
+      new AbortController(),
+    );
+
+    expect(mockShowSnackbar).toHaveBeenCalledTimes(1);
+    expect(mockShowSnackbar).toHaveBeenCalledWith({
+      isLowContrast: true,
+      subtitle: 'It is now visible in the Programs table',
+      kind: 'success',
+      title: 'Program enrollment saved',
+    });
+  });
+
+  xit('updates a program enrollment', async () => {
+    const user = userEvent.setup();
+
+    renderProgramsForm(mockEnrolledProgramsResponse[0].uuid);
+
+    const enrollButton = screen.getByRole('button', { name: /save and close/i });
+    const dateCompletedGroup = screen.getByRole('group', { name: /Date completed/i });
+    const dateCompletedInput = within(dateCompletedGroup).getByRole('textbox');
+
+    mockUpdateProgramEnrollment.mockReturnValueOnce(of({ status: 200, statusText: 'OK' }));
+
+    await user.type(dateCompletedInput, '05/05/2020');
+
+    expect(dateCompletedInput).toHaveValue('05/05/2020');
+
+    await user.tab();
+
+    expect(enrollButton).not.toBeDisabled();
+
+    await user.click(enrollButton);
+
+    expect(mockUpdateProgramEnrollment).toHaveBeenCalledTimes(1);
+    expect(mockUpdateProgramEnrollment).toHaveBeenCalledWith(
+      mockEnrolledProgramsResponse[0].uuid,
+      expect.objectContaining({
+        dateEnrolled: '2020-01-16T00:00:00+00:00',
+        dateCompleted: '2020-05-05T00:00:00+00:00',
+        location: mockEnrolledProgramsResponse[0].location.uuid,
+        patient: mockPatient.id,
+        program: mockEnrolledProgramsResponse[0].program.uuid,
+      }),
+      new AbortController(),
+    );
+
+    expect(mockShowSnackbar).toHaveBeenCalledWith(
+      expect.objectContaining({
+        isLowContrast: true,
+        subtitle: 'Changes to the program are now visible in the Programs table',
+        kind: 'success',
+        title: 'Program enrollment updated',
+      }),
+    );
+  });
+
+  xit('renders an error notification if there was a problem recording a program enrollment', async () => {
+    const user = userEvent.setup();
+
+    const inpatientWardUuid = 'b1a8b05e-3542-4037-bbd3-998ee9c40574';
+    const oncologyScreeningProgramUuid = '11b129ca-a5e7-4025-84bf-b92a173e20de';
+
+    const error = {
+      message: 'Internal Server Error',
+      response: {
+        status: 500,
+        statusText: 'Internal Server Error',
+      },
+    };
+
+    mockOpenmrsFetch.mockReturnValueOnce({ data: { results: mockCareProgramsResponse } });
+    mockOpenmrsFetch.mockReturnValueOnce({ data: { results: mockEnrolledProgramsResponse } });
+    mockCreateProgramEnrollment.mockReturnValueOnce(throwError(error));
+
+    renderProgramsForm();
+
+    const enrollButton = screen.getByRole('button', { name: /save and close/i });
+    const enrollmentDateInput = screen.getAllByRole('textbox', { name: '' })[0];
+    const selectLocationInput = screen.getAllByRole('combobox', { name: '' })[1];
+    const selectProgramInput = screen.getAllByRole('combobox', { name: '' })[0];
+
+    await user.type(enrollmentDateInput, '2020-05-05');
+    await user.selectOptions(selectProgramInput, [oncologyScreeningProgramUuid]);
+    await user.selectOptions(selectLocationInput, [inpatientWardUuid]);
+
+    expect(enrollButton).not.toBeDisabled();
+
+    await user.click(enrollButton);
+
+    expect(mockCreateErrorHandler).toHaveBeenCalledTimes(1);
+    expect(mockShowSnackbar).toHaveBeenCalledWith({
+      isLowContrast: false,
+      subtitle: 'Internal Server Error',
+      kind: 'error',
+      title: 'Error saving program enrollment',
+    });
+  });
+});
+
+function renderProgramsForm(programEnrollmentUuidToEdit?: string) {
+  render(<ProgramsForm {...testProps} programEnrollmentId={programEnrollmentUuidToEdit} />);
+}
