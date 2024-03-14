ee9794753f0687070e8ccb9e45c52864aa31481a
Daphne210 <dapnephinerry2@gmail.com>
2024-03-14 16:39:45 +0300

    Updating with upstream master

--- packages/esm-care-panel-app/src/config-schema.ts
+++ packages/esm-care-panel-app/src/config-schema.ts
@@ -43,7 +43,7 @@ export const configSchema = {
   },
   hivViralLoadDateUuid: {
     _type: Type.ConceptUuid,
-    _default: '0b434cfa-b11c-4d14-aaa2-9aed6ca2da88',
+    _default: ,
   },
   hivViralLoadQualitativeUuid: {
     _type: Type.ConceptUuid,
