76fbfd1d5231cdb9b22ea782eb7fbb2e0b87e570
Daphne210 <dapnephinerry2@gmail.com>
2024-03-20 01:15:36 +0300

    Removing the duplicate imports from react

--- packages/esm-care-panel-app/src/care-panel/care-panel.component.tsx
+++ packages/esm-care-panel-app/src/care-panel/care-panel.component.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { StructuredListSkeleton, ContentSwitcher, Switch } from '@carbon/react';
 import styles from './care-panel.scss';
