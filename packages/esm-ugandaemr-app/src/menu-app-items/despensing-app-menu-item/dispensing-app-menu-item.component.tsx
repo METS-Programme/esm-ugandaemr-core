74aa7183d8ad67742dbc7c3ad9f621b64b1c5d0a
musa-mets <mwanjemusa@gmail.com>
2024-03-21 14:11:15 +0300

    U4X-464:add dispensing app on menu item

---
+++ packages/esm-ugandaemr-app/src/menu-app-items/despensing-app-menu-item/dispensing-app-menu-item.component.tsx
@@ -0,0 +1,17 @@
+import { ClickableTile } from '@carbon/react';
+import React from 'react';
+import styles from './dispensing-app-menu-item.scss';
+import { Medication } from '@carbon/react/icons';
+
+const Item = () => {
+  // items
+  const openmrsSpaBase = window['getOpenmrsSpaBase']();
+
+  return (
+    <ClickableTile className={styles.customTile} id="menu-item" href={`${openmrsSpaBase}dispensing`}>
+      <div className="customTileTitle">{<Medication  size={24} />}</div>
+      <div>Dispensing app</div>
+    </ClickableTile>
+  );
+};
+export default Item;
