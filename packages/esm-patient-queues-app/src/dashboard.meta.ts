7ddc0251933544b0684ec0ec7b751c6adb9dc2e6
AKILENG ISAAC <isaacakileng56@gmail.com>
2024-03-26 20:20:16 +0300

    Revert "U4X-467 :  Create separate Patient-Queues UIs i.e Triage,Reception an…"
    This reverts commit f1837f207f62ca38ca751001925360de6c06d5bb.

--- packages/esm-patient-queues-app/src/dashboard.meta.ts
+++ packages/esm-patient-queues-app/src/dashboard.meta.ts
@@ -3,21 +3,3 @@ export const dashboardMeta = {
   slot: 'patient-queues-dashboard-slot',
   title: 'Patient Queues',
 };
-
-export const ReceptionMeta = {
-  name: 'reception-patient-queues',
-  slot: 'patient-queues-reception-room-dashboard-slot',
-  title: 'Reception',
-};
-
-export const TriageMeta = {
-  name: 'triage-patient-queues',
-  slot: 'patient-queues-triage-room-dashboard-slot',
-  title: 'Triage',
-};
-
-export const ClinicalRoomMeta = {
-  name: 'clinical-room-patient-queues',
-  slot: 'patient-queues-clinical-room-dashboard-slot',
-  title: 'Clinical Room',
-};
--- packages/esm-patient-queues-app/src/home.component.tsx
+++ packages/esm-patient-queues-app/src/home.component.tsx
@@ -1,11 +1,31 @@
+import { UserHasAccess, useSession, userHasAccess } from '@openmrs/esm-framework';
 import React from 'react';
-
+import ActiveVisitsReceptionTable from './active-visit-patient-reception/active-visits-reception-table.component';
+import ActiveVisitsTabs from './active-visits/active-visits-tab.component';
+import {
+  PRIVILEGE_CLINICIAN_QUEUE_LIST,
+  PRIVILEGE_RECEPTION_QUEUE_LIST,
+  PRIVILEGE_TRIAGE_QUEUE_LIST,
+} from './constants';
 import PatientQueueHeader from './patient-queue-header/patient-queue-header.component';
+import ClinicMetrics from './patient-queue-metrics/clinic-metrics.component';
+import QueueLauncher from './queue-launcher/queue-launcher.component';
 
-const Home: React.FC = () => {
+interface HomeProps {}
+
+const Home: React.FC<HomeProps> = (props) => {
+  const session = useSession();
+
   return (
     <div>
       <PatientQueueHeader />
+      <UserHasAccess privilege={PRIVILEGE_RECEPTION_QUEUE_LIST}>
+        <QueueLauncher />
+      </UserHasAccess>
+      <ClinicMetrics />
+      {session?.user && userHasAccess(PRIVILEGE_RECEPTION_QUEUE_LIST, session.user) && <ActiveVisitsReceptionTable />}
+      {session?.user && userHasAccess(PRIVILEGE_TRIAGE_QUEUE_LIST, session.user) && <ActiveVisitsTabs />}
+      {session?.user && userHasAccess(PRIVILEGE_CLINICIAN_QUEUE_LIST, session.user) && <ActiveVisitsTabs />}
     </div>
   );
 };
