960e9b4ee560c0e027502b96cebb32503860f52a
Jabar Jeremy <24471994+jabahum@users.noreply.github.com>
2024-03-28 08:58:58 +0300

    Remove theatre and radiology apps from core (#203)
     Remove theatre and radiology apps from core (#203)

--- packages/esm-theatre-app/src/theatre-app-menu-link.component.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { useTranslation } from 'react-i18next';
-import { ConfigurableLink } from '@openmrs/esm-framework';
-
-export default function TheatreAppMenuLink() {
-  const { t } = useTranslation();
-  return <ConfigurableLink to="${openmrsSpaBase}/theatre">{t('theatre', 'Theatre')}</ConfigurableLink>;
-}
