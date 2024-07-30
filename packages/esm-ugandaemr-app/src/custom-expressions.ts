91285fb4dcd174a0bb3a5a19a0df422dabccdb50
musa-mets <mwanjemusa@gmail.com>
2024-07-30 23:09:52 +0300

    add check for concept data type

--- packages/esm-ugandaemr-app/src/custom-expressions.ts
+++ packages/esm-ugandaemr-app/src/custom-expressions.ts
@@ -1,3 +1,7 @@
+import {fhirBaseUrl, openmrsFetch, restBaseUrl} from "@openmrs/esm-framework";
+import {date} from "zod";
+import dayjs from "dayjs";
+
 export function CalcMonthsOnART(artStartDate: Date, followupDate: Date) {
   let resultMonthsOnART: number;
   let artInDays = Math.round((followupDate.getTime() - artStartDate.getTime?.()) / 86400000);
@@ -8,4 +12,40 @@ export function CalcMonthsOnART(artStartDate: Date, followupDate: Date) {
   }
 
   return artStartDate && followupDate ? resultMonthsOnART : null;
+}
+
+export async function getLatestObs(patientUuid: string, conceptUuid: string, encounterTypeUuid?: string) {
+  let params = `patient=${patientUuid}&code=${conceptUuid}${
+    encounterTypeUuid ? `&encounter.type=${encounterTypeUuid}` : ''
+  }`;
+
+  params += '&_sort=-_lastUpdated&_count=1';
+  const { data } = await openmrsFetch(`${fhirBaseUrl}/Observation?${params}`);
+
+  return data.entry?.length ? data.entry[0].resource : null;
+}
+
+export async function getConceptDataType(conceptUuid: string) {
+  let apiUrl = `${restBaseUrl}/concept/${conceptUuid}?v=custom:(datatype:(name,uuid,display))`;
+
+  const { data } = await openmrsFetch(apiUrl);
+
+  return data?.datatype?.name;
+}
+export async function latestObs(patientId: string, conceptUuid: string) {
+  const response = await getLatestObs(patientId, conceptUuid);
+
+  if (response) {
+    const conceptDataType = await getConceptDataType(conceptUuid);
+
+    if (conceptDataType === 'Date') {
+      return dayjs(response?.valueDateTime).format('DD/MM/YYYY');
+    } else if (conceptDataType === 'Coded') {
+      return response?.valueCodeableConcept?.coding[0]?.display;
+    }else if (conceptDataType === 'Numeric') {
+      return response?.referenceRange[0]?.high?.value;
+    }
+
+
+  }
 }
