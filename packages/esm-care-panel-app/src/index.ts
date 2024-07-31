ad495def670c69dea31c4481b44e7110a3af9e36
Isaac Akileng <isaacakileng56@gmail.com>
2024-07-31 10:02:56 +0300

    Clean up unused code

--- packages/esm-care-panel-app/src/index.ts
+++ packages/esm-care-panel-app/src/index.ts
@@ -1,7 +1,6 @@
 import { getAsyncLifecycle, defineConfigSchema, registerBreadcrumbs, getSyncLifecycle } from '@openmrs/esm-framework';
 import { configSchema } from './config-schema';
-import { dashboardMeta } from './dashboard.meta';
-import { createDashboardLink, registerWorkspace } from '@openmrs/esm-patient-common-lib';
+import { createDashboardLink } from '@openmrs/esm-patient-common-lib';
 
 const moduleName = '@ugandaemr/esm-care-panel-app';
 
@@ -19,21 +18,6 @@ export const carePanelPatientSummary = getAsyncLifecycle(
 );
 export const deleteRegimenConfirmationDialog = getAsyncLifecycle(
   () => import('./regimen-editor/delete-regimen-modal.component'),
-  options,
-);
-registerWorkspace({
-  name: 'patient-regimen-workspace',
-  title: 'Regimen Form',
-  load: getAsyncLifecycle(() => import('./regimen-editor/regimen-form.component'), options),
-  //canMaximize: true,
-  //canHide: true,
-  //width: 'wider',
-  //type: 'clinical-form',
-});
-
-// t('carePanel', 'Care panel')
-export const carePanelSummaryDashboardLink = getSyncLifecycle(
-  createDashboardLink({ ...dashboardMeta, moduleName }),
   options,
 );
 
--- packages/esm-patient-programs-app/README.md
+++
@@ -1,4 +0,0 @@
-# esm-patient-programs-app
-
-The programs widget. It provides a tabular overview of the programs a patient is enrolled into as well as a form for enrolling the patient into new programs.
-
