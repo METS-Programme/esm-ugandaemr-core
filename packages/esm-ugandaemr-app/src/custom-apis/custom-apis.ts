8d47d56e85f7c700162910f3735feecb203ef82c
Isaac Akileng <isaacakileng56@gmail.com>
2024-08-21 14:31:28 +0300

    Add latest approach function

--- packages/esm-ugandaemr-app/src/custom-apis/custom-apis.ts
+++ packages/esm-ugandaemr-app/src/custom-apis/custom-apis.ts
@@ -1,4 +1,7 @@
 import { fhirBaseUrl, restBaseUrl, openmrsFetch } from '@openmrs/esm-framework';
+import useSWR from 'swr';
+import { configSchema } from '@ugandaemr/esm-care-panel-app/src/config-schema';
+import { PatientProgramResponse } from '@ugandaemr/esm-care-panel-app/src/hooks/usePatientPrograms';
 
 export async function getLatestObs(patientUuid: string, conceptUuid: string, encounterTypeUuid?: string) {
   let params = `patient=${patientUuid}&code=${conceptUuid}${
@@ -17,4 +20,12 @@ export async function getConceptDataType(conceptUuid: string) {
   const { data } = await openmrsFetch(apiUrl);
 
   return data?.datatype?.name;
+}
+
+export async function getPatientPrograms(patientUuid: string) {
+  const apiUrl = `${restBaseUrl}/programenrollment/?patient=${patientUuid}&v=custom:(uuid,program:(uuid,name,allWorkflows:(uuid,states:(uuid,concept.name.name,),concept.name.name)),location:(uuid,name),attributes:(uuid,attributeType:(uuid,display),value:(uuid,name.name)),dateEnrolled,dateCompleted,states:(state:(concept),startDate,endDate,voided),outcome)`;
+
+  const { data } = await openmrsFetch(apiUrl);
+
+  return data;
 }
--- packages/esm-ugandaemr-app/src/custom-expressions.ts
+++ packages/esm-ugandaemr-app/src/custom-expressions.ts
@@ -1,5 +1,6 @@
-import { getConceptDataType, getLatestObs } from './custom-apis/custom-apis';
+import { getConceptDataType, getLatestObs, getPatientPrograms } from './custom-apis/custom-apis';
 import dayjs from 'dayjs';
+import { configSchema } from '@ugandaemr/esm-care-panel-app/src/config-schema';
 
 export async function latestObs(patientId: string, conceptUuid: string) {
   const response = await getLatestObs(patientId, conceptUuid);
@@ -13,4 +14,21 @@ export async function latestObs(patientId: string, conceptUuid: string) {
       return response?.valueCodeableConcept?.coding[0]?.display;
     }
   }
+}
+
+export async function patientDSDM(patiendId: string) {
+  const response = await getPatientPrograms(patiendId);
+
+  const dsdmUuids = [
+    configSchema.ccladUuid._default,
+    configSchema.cddpUuid._default,
+    configSchema.fbgUuid._default,
+    configSchema.fbimUuid._default,
+    configSchema.ftdrUuid._default,
+  ];
+
+  const filteredDSDModels =
+    response?.results.flatMap((enrollment) =>
+      enrollment.states.filter((state) => dsdmUuids.includes(state.state.concept.uuid)),
+    ) ?? [];
 }
