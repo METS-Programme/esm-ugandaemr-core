948650877db5583361a73c1025b2228b8d115a01
Mwanje Musa <mwanjemusa@gmail.com>
2024-08-27 12:04:35 +0300

    Custom expression for calc months on ART and numeric datatype logic (#246)
    Custom expression for calc months on ART and numeric datatype logic (#246)

--- packages/esm-ugandaemr-app/src/custom-expressions/custom-expressions.ts
+++ packages/esm-ugandaemr-app/src/custom-expressions/custom-expressions.ts
@@ -12,6 +12,18 @@ export async function latestObs(patientId: string, conceptUuid: string) {
       return dayjs(response?.valueDateTime).format('DD/MM/YYYY');
     } else if (conceptDataType === 'Coded') {
       return response?.valueCodeableConcept?.coding[0]?.display;
+    } else if (conceptDataType === 'Numeric') {
+      const valueQuantity = response?.valueQuantity?.value;
+      const unit = response?.valueQuantity?.unit;
+
+      const referenceRangeLow = response?.referenceRange?.[0]?.low?.value;
+      const referenceRangeHigh = response?.referenceRange?.[0]?.high?.value;
+
+      if (referenceRangeLow !== undefined && referenceRangeHigh !== undefined) {
+        return `${referenceRangeLow} - ${referenceRangeHigh} ${unit || ''}`;
+      } else if (valueQuantity !== undefined) {
+        return `${valueQuantity} ${unit || ''}`;
+      }
     }
   }
 }
@@ -19,6 +31,16 @@ export async function latestObs(patientId: string, conceptUuid: string) {
 export async function patientDSDM(patiendId: string) {
   const DSDModels = await getPatientPrograms(patiendId);
 
-  console.info(DSDModels);
   return DSDModels[0]?.state?.concept?.display;
+}
+
+export function CalcMonthsOnART(artStartDate: Date, followupDate: Date) {
+  let resultMonthsOnART: number;
+  let artInDays = Math.round((followupDate.getTime() - artStartDate.getTime?.()) / 86400000);
+  if (artStartDate && followupDate && artInDays < 30) {
+    resultMonthsOnART = 0;
+  } else if (artStartDate && followupDate && artInDays >= 30) {
+    resultMonthsOnART = Math.floor(artInDays / 30);
+  }
+  return artStartDate && followupDate ? resultMonthsOnART : null;
 }
