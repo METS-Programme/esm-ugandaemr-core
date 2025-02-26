9b1efac0950ddf95673f77cd5733bb32ecee1b0c
jabahum <jabajnan8@gmail.com>
2025-02-26 14:52:50 +0300

    add privilges for viewership of components

---
+++ packages/esm-patient-queues-app/src/config/privileges.ts
@@ -0,0 +1,8 @@
+/** @type {string}: Manage Global Properties, Able to add/edit global properties*/
+export const MANAGE_GLOBAL_PROPERTIES = 'Manage Global Properties';
+
+/** @type {string}: App: patientqueue.triage.dashboard, Able to view patient queue triage application dashboard*/
+export const APP_PATIENTQUEUE_TRIAGE_DASHBOARD = 'App: patientqueue.triage.dashboard';
+
+/** @type {string}: App: patientqueue.clinician.dashboard, Able to view patient queue clinician application dashboard*/
+export const APP_PATIENTQUEUE_CLINICIAN_DASHBOARD = 'App: patientqueue.clinician.dashboard';
