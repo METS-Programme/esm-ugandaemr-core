fd06c874d5273e9d3ec1e145d40e9965e3716a63
jabahum <jabajnan8@gmail.com>
2025-02-27 15:11:26 +0300

    add filter by tag for the currently logged location

--- packages/esm-patient-queues-app/src/config-schema.ts
+++ packages/esm-patient-queues-app/src/config-schema.ts
@@ -1,1 +1,19 @@
-export const configSchema = {};
+import { Type } from '@openmrs/esm-framework';
+
+export const configSchema = {
+  triageRoomTag: {
+    _type: Type.String,
+    _description: 'This is triage room tag',
+    _default: '3e525526-cd66-46ad-96b3-224d46e75676',
+  },
+  clinicalRoomTag: {
+    _type: Type.String,
+    _description: 'This is clinical room tag',
+    _default: '96be1b53-e65c-494b-be41-b36899cb5d09',
+  },
+};
+
+export interface PatientQueueConfig {
+  triageRoomTag: string;
+  clinicalRoomTag: string;
+}
