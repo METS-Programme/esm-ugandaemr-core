c528f7c743add17cf69e5ff9618d06cac45a8b09
Jabar Jeremy <24471994+jabahum@users.noreply.github.com>
2025-03-21 14:10:41 +0300

    Add app menu to  core (#285)
    Add app menu to  core

---
+++ packages/esm-ugandaemr-app/src/overlay/overlay.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+import { isDesktop, useLayoutType } from "@openmrs/esm-framework";
+import styles from "./overlay.scss";
+
+interface OverlayProps {
+  close: () => void;
+  children?: React.ReactNode;
+  header: string;
+  buttonsGroup?: React.ReactElement;
+}
+
+const Overlay: React.FC<OverlayProps> = ({
+  close,
+  children,
+  header,
+  buttonsGroup,
+}) => {
+  const layout = useLayoutType();
+
+  return (
+    <div
+      className={
+        isDesktop(layout) ? styles.desktopOverlay : styles.tabletOverlay
+      }
+    >
+      <div className={styles.overlayContent}>{children}</div>
+      <div className={styles.buttonsGroup}>{buttonsGroup}</div>
+    </div>
+  );
+};
+
+export default Overlay;
