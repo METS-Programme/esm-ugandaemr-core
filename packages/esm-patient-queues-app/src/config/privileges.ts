fc2e858e99fadb2c667c075914cc7ba2239f577d
Daphne <89844768+Daphne210@users.noreply.github.com>
2025-04-04 11:17:05 +0300

    Fix queue and clinician tables (#306)

--- packages/esm-patient-queues-app/src/config/privileges.ts
+++ packages/esm-patient-queues-app/src/config/privileges.ts
@@ -2,7 +2,7 @@
 export const MANAGE_GLOBAL_PROPERTIES = 'Manage Global Properties';
 
 /** @type {string}: App: patientqueue.triage.dashboard, Able to view patient queue triage application dashboard*/
-export const APP_PATIENTQUEUE_TRIAGE_DASHBOARD = 'App: patientqueue.triage.dashboard';
+export const APP_PATIENTQUEUE_TRIAGE_DASHBOARD = 'App: patientQueue.triage.dashboard';
 
 /** @type {string}: App: patientqueue.clinician.dashboard, Able to view patient queue clinician application dashboard*/
-export const APP_PATIENTQUEUE_CLINICIAN_DASHBOARD = 'App: patientqueue.clinician.dashboard';
+export const APP_PATIENTQUEUE_CLINICIAN_DASHBOARD = 'App: patientQueue.clinician.dashboard';
