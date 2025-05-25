6a7f5b113f688478198637fed0d0a6618b869eea
jabahum <jabajnan8@gmail.com>
2025-05-25 09:14:14 +0300

    fix comments

--- packages/esm-patient-queues-app/src/components/actions/death/death-notification-actions-button.component.tsx
+++ packages/esm-patient-queues-app/src/components/actions/death/death-notification-actions-button.component.tsx
@@ -1,6 +1,7 @@
 import { launchWorkspace } from "@openmrs/esm-framework";
 import React, { useCallback } from "react";
 import { useTranslation } from "react-i18next";
+import styles from "./death-notification-actions-button.module.css";
 
 
 const DeathNotificationActionsButton : React.FC = () => {
@@ -9,7 +10,7 @@ const DeathNotificationActionsButton : React.FC = () => {
   const handleLaunchWorkspace = useCallback(() => {
   launchWorkspace('patient-form-entry-workspace', {
     formInfo: {
-      formUuid: '00001ae1-1b37-41ca-adb2-17c04df008da',
+      formUuid: 'bb282ac8-d8b6-4d76-88b9-86da83efec41',
     },
     workspaceTitle: 'Notification and Certification of Death',
   });
@@ -19,14 +20,11 @@ const DeathNotificationActionsButton : React.FC = () => {
   return (
     <li className="cds--overflow-menu-options__option">
       <button
-        className="cds--overflow-menu-options__btn"
+        className={`cds--overflow-menu-options__btn ${styles.markDeadButton}`}
         role="menuitem"
         title={t('markDead', 'Mark Dead')}
         data-floating-menu-primary-focus
         onClick={() => handleLaunchWorkspace()}
-        style={{
-          maxWidth: '100vw',
-        }}
       >
         <span className="cds--overflow-menu-options__option-content">{t('markDead', 'Mark Dead')}</span>
       </button>
