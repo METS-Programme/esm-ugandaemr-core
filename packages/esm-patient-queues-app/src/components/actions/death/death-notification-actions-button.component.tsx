f921055af6130bb3b6f9014f26897f4120fa06f4
Daphne210 <dapnephinerry2@gmail.com>
2025-05-29 11:16:00 +0300

    Changing the naming of the mark dead action button

--- packages/esm-patient-queues-app/src/components/actions/death/death-notification-actions-button.component.tsx
+++ packages/esm-patient-queues-app/src/components/actions/death/death-notification-actions-button.component.tsx
@@ -1,35 +1,35 @@
-import { launchWorkspace } from "@openmrs/esm-framework";
-import React, { useCallback } from "react";
-import { useTranslation } from "react-i18next";
-import styles from "./death-notification-actions.scss";
+import { launchWorkspace } from '@openmrs/esm-framework';
+import React, { useCallback } from 'react';
+import { useTranslation } from 'react-i18next';
+import styles from './death-notification-actions.scss';
 
-
-const DeathNotificationActionsButton : React.FC = () => {
-   const { t } = useTranslation();
+const DeathNotificationActionsButton: React.FC = () => {
+  const { t } = useTranslation();
 
   const handleLaunchWorkspace = useCallback(() => {
-  launchWorkspace('patient-form-entry-workspace', {
-    formInfo: {
-      formUuid: 'bb282ac8-d8b6-4d76-88b9-86da83efec41',
-    },
-    workspaceTitle: 'Notification and Certification of Death',
-  });
-}, []);
-
+    launchWorkspace('patient-form-entry-workspace', {
+      formInfo: {
+        formUuid: 'bb282ac8-d8b6-4d76-88b9-86da83efec41',
+      },
+      workspaceTitle: 'Notification and Certification of Death',
+    });
+  }, []);
 
   return (
     <li className="cds--overflow-menu-options__option">
       <button
         className={`cds--overflow-menu-options__btn ${styles.markDeadButton}`}
         role="menuitem"
-        title={t('markDead', 'Mark Dead')}
+        title={t('markPatientDeceased', 'Mark Patient Deceased')}
         data-floating-menu-primary-focus
         onClick={() => handleLaunchWorkspace()}
       >
-        <span className="cds--overflow-menu-options__option-content">{t('markDead', 'Mark Dead')}</span>
+        <span className="cds--overflow-menu-options__option-content">
+          {t('markPatientDeceased', 'Mark Patient Deceased')}
+        </span>
       </button>
     </li>
   );
-}
+};
 
 export default DeathNotificationActionsButton;
